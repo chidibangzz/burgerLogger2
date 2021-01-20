66f144975ad543c3a9e251cf25e1bde9a67d6d0f
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-01-20 09:45:13 -0600

    deleted if else connection

--- config/connection.js
+++ config/connection.js
@@ -3,18 +3,14 @@
 
 var mysql = require("mysql2");
 
-if (process.env.JAWSDB_URL) {
-  connection = "mysql://s9pkanuohl9onjo8:xotr8gd7gm6nish3@j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ks2wepxti56zt15i"
-} else {
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Maryland123!",
+  database: "burger_db"
+});
 
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Maryland123!",
-    database: "burger_db"
-  });
-}
 
 // Make connection.
 connection.connect(function (err) {
