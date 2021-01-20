d83adaccbec6e15db0d0ee855d578a816189c7a6
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-01-19 18:02:07 -0600

    read me

--- config/connection.js
+++ config/connection.js
@@ -3,6 +3,10 @@
 
 var mysql = require("mysql2");
 
+if(process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -10,6 +14,7 @@ var connection = mysql.createConnection({
   password: "Maryland123!",
   database: "burger_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
