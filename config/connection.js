1aedca60f17cd84a906dd5bb99514f090c7db76a
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-01-20 09:37:40 -0600

    changed connection line

--- config/connection.js
+++ config/connection.js
@@ -3,21 +3,21 @@
 
 var mysql = require("mysql2");
 
-if(process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+if (process.env.JAWSDB_URL) {
+  connection = "mysql://s9pkanuohl9onjo8:xotr8gd7gm6nish3@j21q532mu148i8ms.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/ks2wepxti56zt15i"
 } else {
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Maryland123!",
-  database: "burger_db"
-});
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Maryland123!",
+    database: "burger_db"
+  });
 }
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
