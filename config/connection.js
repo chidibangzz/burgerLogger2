373ea70301b990adc4e4aa27de67cbab94b6e950
Chidi Ekeke <chidiekeke18@yahoo.com>
2021-01-20 18:14:44 -0600

    string

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,19 @@
 // Set up MySQL connection.
 
-
+var connection;
 var mysql = require("mysql2");
-
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  // Database is JawsDB on Heroku
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "Maryland123!",
   database: "burger_db"
 });
-
+};
 
 // Make connection.
 connection.connect(function (err) {
